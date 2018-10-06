268f937c74cbaa105313fa5b4845b0800bb5ebb2
Tobias <42217856+caicedos@users.noreply.github.com>
2018-10-05 21:25:59 -0400

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("HI!!")
+
+// function to display the authentication card
+function displayAuthentication(){
+// the okta script goes here
+}
+
+// on click event listener
+$("#sign-in").on("click", function(event){
+    event.preventDefault();
+
+    displayAuthentication();
+
+});
